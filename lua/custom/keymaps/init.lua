38312195fb4638a75cf4b3e832f2afd9328f3ad9
Joe Barhouch <joe.barhouch@gmail.com>
2024-05-15 22:11:28 +0100

    keymaps file

---
+++ lua/custom/keymaps/init.lua
@@ -0,0 +1,39 @@
+-- Keymaps
+
+--[[Insert Quarto Blocks]]
+local is_code_chunk = function()
+  local current, _ = require('otter.keeper').get_current_language_context()
+  if current then
+    return true
+  else
+    return false
+  end
+end
+
+--- Insert code chunk of given language
+--- Splits current chunk if already within a chunk
+--- @param lang string
+local insert_code_chunk = function(lang)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'n', true)
+  local keys
+  if is_code_chunk() then
+    keys = [[o```<cr><cr>```{]] .. lang .. [[}<esc>o]]
+  else
+    keys = [[o```{]] .. lang .. [[}<cr>```<esc>O]]
+  end
+  keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+  vim.api.nvim_feedkeys(keys, 'n', false)
+end
+
+local insert_py_chunk = function()
+  insert_code_chunk 'python'
+end
+
+--[[Keymaps]]
+vim.keymap.set(
+  -- Insert Quarto Blocks
+  'n',
+  '<c-p>',
+  insert_py_chunk,
+  { noremap = true, silent = true }
+)
